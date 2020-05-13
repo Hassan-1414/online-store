f277f716243a1c9e06c97620737c45d080b92635
hassan <HassanMohsen1414@gmail.com>
2020-05-14 00:36:08 +0200

    adding routing

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import "./homepage.scss";
 import Directory from "../../components/directory/Directory";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Yo from Hats Page</h1>
+    </div>
+  );
+};
 
 const HomePage = () => {
   return (
     <div className="homepage">
-      <Directory />
+      <Switch>
+        <Route exact path="/" component={Directory} />
+        <Route exact path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 };
